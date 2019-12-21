877487923d1def9ea4262fba926f9ba8aecc9b22
Aananth <aananthv@outlook.com>
2019-12-21 19:36:21 +0530

    Clean Uplaod

--- Server/app.js
+++ Server/app.js
@@ -10,7 +10,7 @@ const fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
-var usersRouter = require('./routes/users');
+var uploadRouter = require('./routes/upload');
 
 var app = express();
 
@@ -28,47 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
-app.use('/users', usersRouter);
+app.use('/', uploadRouter);
 
-app.use(fileUpload());
-app.get('/login', function(req, res, next) {
-	res.render('login');
-});
-
-app.get('/register', function(req, res, next) {
-	res.render('register');
-});
-
-app.get('/upload', function(req, res, next) {
-	res.render('upload');
-});
-
-// default options
-
-app.post('/upload', function(req, res) {
-	console.log('upload post');
-	console.log(req.files);
-
-	if (!req.files || Object.keys(req.files).length === 0) {
-		return res.status(400).send('No files were uploaded.');
-	}
-
-	// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-	let sampleFile = req.files.sampleFile;
-
-	console.log(sampleFile);
-
-	// Use the mv() method to place the file somewhere on your server
-	sampleFile.mv(`./files/${sampleFile.name}`, function(err) {
-		if (err) return res.status(500).send(err);
-
-		sha256File(`./files/${sampleFile.name}`, (error, sum) => {
-			if (error) return console.log(error);
-			console.log(sum);
-			return res.status(200).send(sum);
-		});
-	});
-});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	next(createError(404));
---
+++ Server/routes/upload.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var sha256File = require('sha256-file');
+const fileUpload = require('express-fileupload');
+
+router.use(fileUpload());
+
+router.get('/upload', function(req, res, next) {
+	res.render('upload');
+});
+
+// default options
+
+router.post('/upload', function(req, res) {
+	console.log('upload post');
+	console.log(req.files);
+
+	if (!req.files || Object.keys(req.files).length === 0) {
+		return res.status(400).send('No files were uploaded.');
+	}
+
+	// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+	let sampleFile = req.files.sampleFile;
+
+	console.log(sampleFile);
+
+	// Use the mv() method to place the file somewhere on your server
+	sampleFile.mv(`./files/${sampleFile.name}`, function(err) {
+		if (err) return res.status(500).send(err);
+
+		sha256File(`./files/${sampleFile.name}`, (error, sum) => {
+			if (error) return console.log(error);
+			console.log(sum);
+			return res.status(200).send(sum);
+		});
+	});
+});
+
+module.exports = router;
