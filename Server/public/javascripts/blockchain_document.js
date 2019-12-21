cc3a5a6cdcd419e94ee33d3dfa0e697ff2ce6d10
bestin <bestinbthomas@gmail.com>
2019-12-21 23:45:40 +0530

    hncfwkh

--- Server/public/javascripts/blockchain_document.js
+++ Server/public/javascripts/blockchain_document.js
@@ -1,24 +1,38 @@
 // user 0x538db49a39b90Eb679534f648318dF0215DD493E
 // doc 0xDaD9241dEc0BB929d471E15C6E960d0E2AF735E1
 
-let addDocToBC = function (hash, signature) {
-    $.get('artifacts/DigitalSignature.json', function (response) {
-        const DigitalSignature = window.web3.eth.contract(response.abi).at("0xDaD9241dEc0BB929d471E15C6E960d0E2AF735E1");
-        DigitalSignature.addDocument.sendTransaction(hash, signature, {
-            from: window.web3.eth.accounts[0]
-        }, function (error, result) {
-            console.log(result);
-        })
-    })
-}
+let addDocToBC = function(hash, signature) {
+	$.get('artifacts/DigitalSignature.json', function(response) {
+		const DigitalSignature = window.web3.eth
+			.contract(response.abi)
+			.at('0xDaD9241dEc0BB929d471E15C6E960d0E2AF735E1');
+		DigitalSignature.addDocument.sendTransaction(
+			hash,
+			signature,
+			{
+				from : window.web3.eth.accounts[0]
+			},
+			function(error, result) {
+				console.log(result);
+			}
+		);
+	});
+};
 
-let addSignature = function (hash, signature) {
-    $.get('artifacts/DigitalSignature.json', function (response) {
-        const DigitalSignature = window.web3.eth.contract(response.abi).at("0xDaD9241dEc0BB929d471E15C6E960d0E2AF735E1");
-        DigitalSignature.addSignature.sendTransaction(hash, signature, {
-            from: window.web3.eth.accounts[0]
-        }, function (error, result) {
-            console.log(result);
-        })
-    })
-}
+let addSignature = function(hash, signature) {
+	$.get('artifacts/DigitalSignature.json', function(response) {
+		const DigitalSignature = window.web3.eth
+			.contract(response.abi)
+			.at('0xDaD9241dEc0BB929d471E15C6E960d0E2AF735E1');
+		DigitalSignature.addSignature.sendTransaction(
+			hash,
+			signature,
+			{
+				from : window.web3.eth.accounts[0]
+			},
+			function(error, result) {
+				console.log(result);
+			}
+		);
+	});
+};
