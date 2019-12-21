16d6bc099951d330b9285b75d18241d81c50b1b4
bestin <bestinbthomas@gmail.com>
2019-12-22 03:40:05 +0530

    reg sucess

--- Server/public/javascripts/blockchain_user.js
+++ Server/public/javascripts/blockchain_user.js
@@ -1,11 +1,16 @@
-var addUserToBC = function (user_id, user_name) {
-  $.get('http://localhost:3000/artifacts/User.json', function (response) {
-    const UserContract = window.web3.eth.contract(response.abi).at("0xc8fEb1931EB3b70F03e432745FCf45B2490c5F89");
-    UserContract.setUser.sendTransaction(user_id, user_name, {
-      from: web3.eth.accounts[0]
-    }, function (error, result) {
-      console.log(result);
-      window.location = "http://localhost:3000/dashboard";
-    })
-  })
-}
+var addUserToBC = function(user_id, user_name) {
+	$.get('http://localhost:3000/artifacts/User.json', function(response) {
+		const UserContract = window.web3.eth.contract(response.abi).at('0xc8fEb1931EB3b70F03e432745FCf45B2490c5F89');
+		UserContract.setUser.sendTransaction(
+			user_id,
+			user_name,
+			{
+				from : web3.eth.accounts[0]
+			},
+			function(error, result) {
+				console.log(result);
+				window.location = 'http://localhoast:3000/registration_success/' + result;
+			}
+		);
+	});
+};
--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-var authController = require('../controllers/auth.js')
+var authController = require('../controllers/auth.js');
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
-  res.render('login');
+	res.render('login');
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register');
+	res.render('register');
 });
 
-router.post('/register', authController.register)
+router.get('/registration_success/:tx_hash', function(req, res, next) {
+	res.render('register_sucess', { tx_hash: req.params.tx_hash });
+});
+router.post('/register', authController.register);
 
-router.post('/login', authController.login)
+router.post('/login', authController.login);
 
 module.exports = router;
