3dcef981895dad1c101d284351638de089f3fefe
Aananth <aananthv@outlook.com>
2019-12-21 20:03:46 +0530

    Fix web3js

--- Server/public/javascripts/register.js
+++ Server/public/javascripts/register.js
@@ -1,7 +1,7 @@
 function registerUser() {
   var data = $('form').serialize();
   console.log($('form').name)
-  data += '&public_address=' + web3js.eth.accounts[0];
+  data += '&public_address=' + window.web3.eth.accounts[0];
   console.log(data);
   $.post('register', data, function(documentId) {
     addUserToBC(documentId, $('form').name)
