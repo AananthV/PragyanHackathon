58a1744a57b3a132e4fd8c70ee98868464cf0074
Aananth <aananthv@outlook.com>
2019-12-21 18:46:17 +0530

    WOOOOHOOOOOO

--- Server/public/javascripts/blockchain.js
+++ Server/public/javascripts/blockchain.js
@@ -6,11 +6,17 @@ window.addEventListener('load', async () => {
       // Request account access if needed
       await ethereum.enable();
       // Acccounts now exposed
-      console.log("Accounts exposed")
+      console.log("Accounts Enabled")
     } catch (error) {
       // User denied account access...
     }
   }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    window.web3 = new Web3(web3.currentProvider);
+    // Acccounts always exposed
+  }
+  // Non-dapp browsers...
   else {
     console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
   }
--- Server/public/javascripts/blockchain_user.js
+++ Server/public/javascripts/blockchain_user.js
@@ -1,8 +1,10 @@
 var addUserToBC = function(user_id, user_name) {
   $.get('artifacts/User.json', function(response) {
-    const UserContract = new window.web3.eth.Contract(response.abi, "0xfD18c94162217b6DF78226632A55F9E08c082975");
+    const UserContract = window.web3.eth.contract(response.abi).at("0xfD18c94162217b6DF78226632A55F9E08c082975");
 
-    UserContract.methods.setUser(user_id, user_name).send()
+    UserContract.setUser.sendTransaction(user_id, user_name, {from: web3.eth.accounts[0]}, function (error, result) {
+      console.log(result);
+    })
   })
 }
 // Doc 0xBC28FFFAa9d5BfEb74Bb5466500361513c888CC6
