4f9dde54b38ad3a96fc2fd2d1b281d030ee16f56
bestin <bestinbthomas@gmail.com>
2019-12-21 15:16:04 +0530

    xtrscygjkl

--- Server/app.js
+++ Server/app.js
@@ -22,32 +22,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-router.get('/login', function(req, res, next) {
-  res.render('login');
+app.get('/login', function(req, res, next) {
+	res.render('login');
 });
 
-router.get('/register', function(req, res, next) {
-  res.render('register');
+app.get('/register', function(req, res, next) {
+	res.render('register');
 });
 
-router.get('/upload', function(req, res, next) {
-  res.render('upload');
+app.get('/upload', function(req, res, next) {
+	res.render('upload');
 });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
